269dd9e056e809cc6e3f03bcb64499bf140bac2d
Starling Germosen <sgermosen@outlook.com>
2024-10-15 20:20:07 -0400

    more styling

--- src/P2/Tuesday/PaqJet/PaqJet.Web/Models/EditCustomerResponse.cs
+++ src/P2/Tuesday/PaqJet/PaqJet.Web/Models/EditCustomerResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace PaqJet.Web.Models
 {
-    public class EditCustomerRequest : CustomerViewModel
+    public class EditCustomerResponse : CustomerViewModel
     {
         public int Id { get; set; }
         //public string Name { get; set; }
