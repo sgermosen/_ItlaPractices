71dd8ccadc6f481117f905df6849854b8ed63dac
Starling Germosen <sgermosen@outlook.com>
2025-02-24 20:51:35 -0400

    Update Program.cs

--- src/P1/Friday/MyChambas/MyChamba5/Program.cs
+++ src/P1/Friday/MyChambas/MyChamba5/Program.cs
@@ -1,95 +1,97 @@
-﻿
 List<decimal> typedNumbers = new List<decimal>();
 
 decimal result = 0;
 int typedOption = 1;
 int wantToContinue = 0;
+bool running = true;
 
-DisplayHeader();
 
-try
+Console.WriteLine("This is the best calculator");
+
+while (running)
 {
-    typedOption = Convert.ToInt32(Console.ReadLine());
+    DisplayHeader();
 
-    Console.WriteLine("Please Type the first number");
-    typedNumbers.Add(Convert.ToDecimal(Console.ReadLine()));
-
-    Console.WriteLine("Please Type the second number");
-    typedNumbers.Add(Convert.ToDecimal(Console.ReadLine()));
-
-    while (wantToContinue != 2)
+    try
     {
-        Console.WriteLine("you want to continue inserting numbers? 1. Yes, 2. No");
-        wantToContinue = Convert.ToInt32(Console.ReadLine());
-        if (wantToContinue == 1)
-        {
-            int quantityElementsInTheArray = typedNumbers.Count + 1;
+        typedOption = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please Type another number");
+        if (typedOption == 5)
+        { running = false; }
+        else
+        {
+            Console.WriteLine("Please Type the first number");
             typedNumbers.Add(Convert.ToDecimal(Console.ReadLine()));
+
+            Console.WriteLine("Please Type the second number");
+            typedNumbers.Add(Convert.ToDecimal(Console.ReadLine()));
+
+            while (wantToContinue != 2)
+            {
+                Console.WriteLine("you want to continue inserting numbers? 1. Yes, 2. No");
+                wantToContinue = Convert.ToInt32(Console.ReadLine());
+                if (wantToContinue == 1)
+                {
+                    Console.WriteLine("Please Type another number");
+                    typedNumbers.Add(Convert.ToDecimal(Console.ReadLine()));
+                }
+            }
+
+            switch (typedOption)
+            {
+                case 1:
+                    {
+                        result = AddList(typedNumbers);
+                    }
+                    break;
+                case 2:
+                    foreach (int typedNumber in typedNumbers)
+                    {
+                        result -= typedNumber;
+                    }
+
+                    break;
+                case 3:
+                    foreach (int typedNumber in typedNumbers)
+                    {
+                        result *= typedNumber;
+                    }
+
+                    break;
+                case 4:
+                    foreach (int typedNumber in typedNumbers)
+                    {
+                        result /= typedNumber;
+                    }
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+
+            Console.WriteLine($"The Result of the operation is:{result}");
+
 
         }
     }
-
-    switch (typedOption)
+    catch (DivideByZeroException ex)
     {
-        case 1:
-            {
-                //foreach (int typedNumber in typedNumbers)
-                //{
-                //    result = Add(result, typedNumber);
-                //   // AddByRef(ref result, typedNumber);
-                //    // result += typedNumber;
-                //}
-                result = AddList(typedNumbers);
-            }
-            break;
-        case 2:
-            foreach (int typedNumber in typedNumbers)
-            {
-                result -= typedNumber;
-            }
-
-            break;
-        case 3:
-            foreach (int typedNumber in typedNumbers)
-            {
-                result *= typedNumber;
-            }
-
-            break;
-        case 4:
-            foreach (int typedNumber in typedNumbers)
-            {
-                result /= typedNumber;
-            }
-            break;
-        default:
-            result = 0;
-            break;
+        Console.WriteLine($"you can not divide by zero: {ex.Message}");
+    }
+    catch (ArithmeticException ex)
+    {
+        Console.WriteLine($"you can not divide by zero: {ex.Message}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"You need to choose a correct option: {ex.Message}");
+    }
+    finally
+    {
+        Console.WriteLine("Closing Db Conection");
     }
 
-    Console.WriteLine($"The Result of the operation is:{result}");
-
-
 }
-catch (DivideByZeroException ex)
-{
-    Console.WriteLine($"you can not divide by zero: {ex.Message}");
-}
-catch (ArithmeticException ex)
-{
-    Console.WriteLine($"you can not divide by zero: {ex.Message}");
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"You need to choose a correct option: {ex.Message}");
-}
-finally
-{
-    Console.WriteLine("Closing Db Conection");
-}
-
 //procceess
 static void AddByRef(ref decimal valueToModify, decimal value)
 {
@@ -107,9 +109,9 @@ static decimal AddList(List<decimal> typedNumbers)
 {
     decimal result = 0;
     foreach (int typedNumber in typedNumbers)
-    { 
+    {
         result = Add(result, typedNumber);
-       // AddByRef(ref result, typedNumber);
+        // AddByRef(ref result, typedNumber);
         // result += typedNumber;
     }
     return result;
@@ -117,8 +119,7 @@ static decimal AddList(List<decimal> typedNumbers)
 
 static void DisplayHeader()
 {
-    Console.WriteLine("This is the best calculator");
     Console.WriteLine("Please Type the option number than you want");
     Console.WriteLine("---------------------------------------");
-    Console.WriteLine("1. Sum, \n 2. Substract,  \n 3. Multiplication,  \n 4. Division,  \n 5. Exit");
+    Console.WriteLine("1. Sum, \n2. Substract,  \n3. Multiplication,  \n4. Division,  \n5. Exit");
 }
