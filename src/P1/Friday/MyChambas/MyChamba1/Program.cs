18f6defc09e39930d9fe13f4854bcb52e4da78c4
Starling Germosen <sgermosen@outlook.com>
2025-05-23 20:43:33 -0400

    Update Program.cs

--- src/P1/Friday/MyChambas/MyChamba1/Program.cs
+++ src/P1/Friday/MyChambas/MyChamba1/Program.cs
@@ -174,16 +174,16 @@ var num2 = 2;
 var num3 = 2;
 var num4 = 2;
 var num5 = 2;
-
+ 
 var result7 = num1 + num2 * num3 * num4 ^ num5 + num2 - num5 * num4 % num5;
-//result7 = num1 + num2 * num3 / numtemp1 + num2 - num5 * num4 % num5;
-//result7 = num1 + num2 * num3 / numtemp1 + num2 - num5 * num4 % num5;
-//result7 = num1 + numtemp2 / numtemp1 + num2 - num5 * num4 % num5;
-//result7 = num1 + numtemp2 / numtemp1 + num2 - num5 * numtemp3;
-//result7 = num1 + numtemp4 + num2 - num5 * numtemp3;
-//result7 = num1 + numtemp4 + num2 - numtemp5;
-//result7 = numtemp6 + numtemp7;
-//result7 = numtemp8;
+////result7 = num1 + num2 * num3 * numtemp1 + num2 - num5 * num4 % num5; 
+////result7 = num1 + numtemp2 * numtemp1 + num2 - num5 * num4 % num5;
+////result7 = num1 + numtemp2 * numtemp1 + num2 - num5 * numtemp3;
+////result7 = num1 + numtemp4 + num2 - num5 * numtemp3;
+////result7 = num1 + numtemp4 + num2 - numtemp5;
+////result7 = numtemp6 + num2 - numtemp5;
+////result7 = numtemp7 - numtemp5;
+////result7 = numtemp8;
 
 var result8= ((num1 + ((num2 * num3) * (num4 ^ num5 )) ) + (num2 - (num5 * (num4 % num5))));
 var result9= ((num1 + (num2 * num3 * (num4 ^ num5 )) ) + (num2 - (num5 * (num4 % num5))));
